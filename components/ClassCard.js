d873265283c6ea8d063e014bc5a52de0b937a2d0
Hemant Bhandari <zenithtaiga@gmail.com>
2023-02-26 05:06:54 +0530

    Increase border size and hover

--- components/ClassCard.js
+++ components/ClassCard.js
@@ -1,7 +1,7 @@
 export default function ClassCard(props) {
   return (
     <>
-      <div className="card card-bordered w-56 bg-base-100 shadow-xl hover:cursor-pointer">
+      <div className="card card-bordered border-2 shadow-xl hover:shadow-inner w-56 bg-base-100 hover:cursor-pointer">
         <div className="card-body items-center text-center hover:text-secondary-focus">
           <h2 className="card-title">Class <span className="text-secondary">{ props.children }</span></h2>
         </div>
